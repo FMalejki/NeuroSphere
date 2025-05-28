8956d9f2b214f38c38501290e01d9fb686ab68aa
Filip Sokołowski <123462364+Sok205@users.noreply.github.com>
2025-05-28 19:12:08 +0200

    Feature: Added handlers for images, text, pdfs and zip files. Todo: Connect this functionality with frontend

--- backend/app/services/send_request_to_ai_api.py
+++ backend/app/services/send_request_to_ai_api.py
@@ -3,23 +3,45 @@ from typing import Dict, List, Any
 
 from app.models.prompt_model import PromptModel
 from app.services.openai_service import build_payload, parse_response
-
 from app.services.user_conversation_message_adder import add_message_to_conversation
-
-
 from app.services.openai_service import send_to_openai, send_to_gemini, send_to_hugging_face
+from app.handlers.file_handlers import handle_image, handle_text, handle_pdf, handle_zip, handle_default
 
-
+#TODO: Refactor this so it works with photos and files
 async def send_request_to_ai_api(
     prompts_data: List[PromptModel],
     model_info: str,
     user_message: str,
     user_id: str,
     conversation_id: str,
+    files: List[Any] = None
 ) -> Dict[str, Any]:
     try:
         print("inside send_request_to_ai_api")
+
+        # Structure that will store our request
+        content = [{"type": "text", "text": user_message}]
+
+        # Including files in the request. If files are provided.
+        if files:
+            for f in files:
+                if f.info.type.startswith("image/"):
+                    handle_image(f, content)
+                elif f.info.type == "text/plain":
+                    handle_text(f, content)
+                elif f.info.type == "application/pdf":
+                    handle_pdf(f, content)
+                elif f.info.type == "application/zip":
+                    handle_zip(f, content)
+                else:
+                    throw: NameError(f"Unsupported file type: {f.info.type}") # type: ignore
+
         full_prompt = "\n\n".join([prompt.text for prompt in prompts_data]) + f"\n\n{user_message}"
+
+        # Adding the content to the full prompt
+        if len(content) == 1 and content[0]['type'] == 'text':
+            full_prompt += f"\n\n{content[0]['text']}"
+
         asyncio.create_task(add_message_to_conversation(conversation_id, user_message, "user"))
         # Wybierz odpowiednie API na podstawie api_choice
         if model_info == "openai":
